09ab4107483fec8be6abfbab02136f8ab1e4a98f
nicol <nicol@ANTEC>
2026-07-10 08:03:34 +0200

    updates

--- src/main/java/org/api/cardnexus/services/Tester.java
+++ src/main/java/org/api/cardnexus/services/Tester.java
@@ -1,49 +1,37 @@
 package org.api.cardnexus.services;
 
-import java.util.List;
+import java.time.LocalDate;
 
-import org.api.cardnexus.api.ListingsApi;
-import org.api.cardnexus.client.ApiClient;
+import org.api.cardnexus.api.PricingApi;
 import org.api.cardnexus.client.ApiException;
 import org.api.cardnexus.client.Configuration;
 import org.api.cardnexus.client.auth.HttpBearerAuth;
-import org.api.cardnexus.model.CardCondition;
 
 public class Tester {
 
     	public static void main(String[] args) {
-    	  ApiClient defaultClient = Configuration.getDefaultApiClient();
+    	    var defaultClient = Configuration.getDefaultApiClient();
     	    defaultClient.setBasePath("https://public-api.cardnexus.com/v1");
-    	    
-    	    // Configure HTTP bearer authorization: bearerAuth
-    	    HttpBearerAuth bearerAuth = (HttpBearerAuth) defaultClient.getAuthentication("bearerAuth");
-    	    bearerAuth.setBearerToken("<your token>");
+    	    ((HttpBearerAuth) defaultClient.getAuthentication("bearerAuth")).setBearerToken("your token");
 
-    	    ListingsApi apiInstance = new ListingsApi(defaultClient);
-    	    
-    	    String cursor = "cursor_example"; // String | 
-    	    Integer limit = 50; // Integer | 
-    	    String game = "game_example"; // String | 
-    	    List<Object> productId = null; // List<Object> | 
-    	    CardCondition condition = CardCondition.fromValue("NM"); // CardCondition | 
-    	    String language = "language_example"; // String | 
-    	    String finish = "Standard"; // String | 
-    	    Boolean graded = true; // Boolean | 
-    	    String customId = "customId_example"; // String | 
-    	    String customIdPrefix = "customIdPrefix_example"; // String | 
-    	    String customIdContains = "customIdContains_example"; // String | 
-    	    String commentContains = "commentContains_example"; // String | 
-    	    try {
-    	      var result = apiInstance.listingsList(cursor, limit, game, productId, condition, language, finish, graded, customId, customIdPrefix, customIdContains, commentContains);
-    	      System.out.println(result);
-    	    } catch (ApiException e) {
-    	      System.err.println("Exception when calling ListingsApi#listingsList");
-    	      System.err.println("Status code: " + e.getCode());
-    	      System.err.println("Reason: " + e.getResponseBody());
-    	      System.err.println("Response headers: " + e.getResponseHeaders());
-    	      e.printStackTrace();
-    	    }
-    	  }
-	}
+    	var apiInstance = new PricingApi(defaultClient);
+        Object productId = 50212; 
+        String marketplace = "cardmarket";  
+        String finish = "Standard";  
+        var from = LocalDate.of(2026,6,1);  
+        var to = LocalDate.now();  
+        try {
+          var result = apiInstance.pricingGetHistory(productId, marketplace, finish, from, to);
+          System.out.println(result);
+        } catch (ApiException e) {
+          System.err.println("Exception when calling PricingApi#pricingGetHistory");
+          System.err.println("Status code: " + e.getCode());
+          System.err.println("Reason: " + e.getResponseBody());
+          System.err.println("Response headers: " + e.getResponseHeaders());
+          e.printStackTrace();
+        }
+      }
+
+}
     
 
