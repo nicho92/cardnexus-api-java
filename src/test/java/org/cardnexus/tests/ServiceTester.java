cad3675d3c6893307793f6c7df1a1acd4005257a
nicol <nicol@host.docker.internal>
2026-07-13 22:11:43 +0200

    updates tests

---
+++ src/test/java/org/cardnexus/tests/ServiceTester.java
@@ -0,0 +1,32 @@
+package org.cardnexus.tests;
+
+import java.io.IOException;
+
+import org.api.cardnexus.configuration.NexusConfig;
+import org.api.cardnexus.model.CardProduct;
+import org.api.cardnexus.model.enums.EnumProductType;
+import org.api.cardnexus.model.requests.SearchProductRequest;
+import org.api.cardnexus.services.ProductsService;
+import org.junit.jupiter.api.Test;
+
+
+public class ServiceTester{
+    	@Test
+    	public void testSearchProduct() throws IOException {
+	    
+		NexusConfig.loadTokenFromEnv();
+		
+		var service = new ProductsService();
+		
+		var req = new SearchProductRequest();
+			req.setGame("mtg");
+			req.setName("Thanos, the Mad Titan");
+			req.setStrictTerms(true);
+			req.setProductTypes(EnumProductType.card);
+			
+		service.searchProduct(req).forEach(p->{
+		    var card = (CardProduct)p;
+		    System.out.println(card.getName() + " " + card.getExpansion()+"/"+card.getPrintNumber());
+		});
+	}
+}
