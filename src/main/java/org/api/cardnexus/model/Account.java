0f51b3b7c2819d2e40d559483454dd5cb56e2d36
nicol <nicol@host.docker.internal>
2026-07-16 08:58:23 +0200

    updates

--- src/main/java/org/api/cardnexus/model/Account.java
+++ src/main/java/org/api/cardnexus/model/Account.java
@@ -4,9 +4,5 @@ import java.util.Date;
 
 public record Account (String username,String email,String imageUrl,Date createdAt,Seller seller)
 {
-    
-    @Override
-    public String toString() {
-          return username();
-    }
+ 
 }
--- src/main/java/org/api/cardnexus/model/requests/AbstractGetRequest.java
+++ src/main/java/org/api/cardnexus/model/requests/AbstractGetRequest.java
@@ -10,48 +10,46 @@ import java.util.StringJoiner;
 import org.api.cardnexus.configuration.NexusConfig;
 
 public abstract class AbstractGetRequest {
-	private int limit=100;
-	
-	public int getLimit() {
-		return limit;
-	}
-	public void setLimit(int limit) {
-		this.limit = limit;
-	}
-	
-    private String encode(String value) {
-        return URLEncoder.encode(value, StandardCharsets.UTF_8);
+    private int limit = 100;
+
+    public int getLimit() {
+	return limit;
     }
-    
+
+    public void setLimit(int limit) {
+	this.limit = limit;
+    }
+
+    private String encode(String value) {
+	return URLEncoder.encode(value, StandardCharsets.UTF_8);
+    }
+
     public String toQueryString() {
 
-        try {
+	try {
 
-            var joiner = new StringJoiner("&");
+	    var joiner = new StringJoiner("&");
 
-            for (var pd : Introspector.getBeanInfo(getClass(), Object.class).getPropertyDescriptors()) 
-            {
-                var getter = pd.getReadMethod();
-                if (getter == null) {
-                    continue;
-                }
-                var value = getter.invoke(this);
+	    for (var pd : Introspector.getBeanInfo(getClass(), Object.class).getPropertyDescriptors()) {
+		var getter = pd.getReadMethod();
+		if (getter == null) {
+		    continue;
+		}
+		var value = getter.invoke(this);
 
-                if (value != null) 
-                {
+		if (value != null) {
 		    if (value instanceof LocalDate d)
 			joiner.add(encode(pd.getName()) + "="+ encode(DateTimeFormatter.ofPattern(NexusConfig.REQ_DATE_PATTERN).format(d)));
 		    else
 			joiner.add(encode(pd.getName()) + "=" + encode(value.toString()));
-                }
-            }
+		}
+	    }
 
-            return joiner.toString();
+	    return joiner.toString();
 
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+	} catch (Exception e) {
+	    throw new RuntimeException(e);
+	}
     }
-    
-    
+
 }
