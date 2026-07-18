5d8b12b0204625bc88c79d0acecbcff51879c443
nicol <nicol@host.docker.internal>
2026-07-19 00:19:56 +0200

    updates

--- src/test/java/org/cardnexus/tests/operations/InventorySearch.java
+++ src/test/java/org/cardnexus/tests/operations/InventorySearch.java
@@ -3,6 +3,7 @@ package org.cardnexus.tests.operations;
 import java.io.IOException;
 
 import org.api.cardnexus.configuration.NexusConfig;
+import org.api.cardnexus.model.CardProduct;
 import org.api.cardnexus.model.requests.InventoryRequest;
 import org.api.cardnexus.model.requests.SearchProductRequest;
 import org.api.cardnexus.services.InventoryService;
@@ -18,7 +19,7 @@ class InventorySearch {
     void searchInventoryCardName() throws IOException
     {
 	
-	String search ="Swamp";
+	String search ="Lion's Eye Diamond";
 	
 	NexusConfig.loadTokenFromEnv();
 	NexusConfig.DEFAULT_GAME_VALUE="mtg";
@@ -31,15 +32,26 @@ class InventorySearch {
 	req2.setName(search);
 	req2.setStrictTerms(true);
 
+	var products = pService.searchProduct(req2);
+	
 	var req = new InventoryRequest();
-	req.setProductIds(pService.searchProduct(req2).stream().map(ap->ap.getId()).toList());
+	req.setProductIds(products.stream().map(ap->ap.getId()).toList());
 	
 	var lines = iService.getInventoryLines(req);
 	
-	System.out.println("found "+lines.size() +" in inventory");
-	
-	
-	lines.forEach(System.out::println);
+	lines.forEach(il->{
+	    
+	    var p = (CardProduct)products.stream().filter(ap->ap.getId()==il.productId()).findFirst().get();
+	    
+	    	    
+	    System.out.println(p.getName() + " " + p.getExpansion().code());
+	    System.out.println(il);
+	    System.out.println(p.getPricesByFinish().get(il.finish()));
+	    
+	    System.out.println("=================================");
+	    
+	    
+	});
     }
     
 }
