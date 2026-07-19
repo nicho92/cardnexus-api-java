27063cb696652beec9ebc58f0bb982337d1e13ff
nicol <nicol@host.docker.internal>
2026-07-19 16:01:23 +0200

    add sales management in OrderServices

---
+++ src/main/java/org/api/cardnexus/model/enums/EnumCancelReasons.java
@@ -0,0 +1,9 @@
+package org.api.cardnexus.model.enums;
+
+public enum EnumCancelReasons {
+
+    item_unavailable,
+    item_damaged,
+    pricing_error,
+    seller_other
+}
--- src/main/java/org/api/cardnexus/services/OrdersService.java
+++ src/main/java/org/api/cardnexus/services/OrdersService.java
@@ -6,7 +6,10 @@ import java.util.List;
 
 import org.api.cardnexus.model.Order;
 import org.api.cardnexus.model.Pagination;
+import org.api.cardnexus.model.enums.EnumCancelReasons;
 import org.api.cardnexus.model.requests.SalesRequest;
+
+import com.google.gson.JsonObject;
 
 public class OrdersService extends AbstractNexusService {
 
@@ -29,6 +32,24 @@ public class OrdersService extends AbstractNexusService {
     {
 	 return client.get(ROOT_SALES_ENDPOINT+"/"+orderNumber,Order.class);
     }
+    
+    public Order markAsShipped(String orderNumber, String trackNumber) throws IOException
+    {
+	var obj = new JsonObject();
+		obj.addProperty("trackingNumber", trackNumber);
+	
+	 return client.post(ROOT_SALES_ENDPOINT+"/"+orderNumber+"/mark-shipped",obj,Order.class);
+    }
+    
+    public Order markAsCanceled(String orderNumber, EnumCancelReasons reason, String comment) throws IOException
+    {
+	var obj = new JsonObject();
+		obj.addProperty("reason", reason.name());
+		obj.addProperty("reasonText", comment)	;
+	
+	 return client.post(ROOT_SALES_ENDPOINT+"/"+orderNumber+"/cancel",obj,Order.class);
+    }
+    
     
     public Order getPurchase(String orderNumber) throws IOException
     {
