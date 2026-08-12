291f5f1310cd08ba47e7d29f9fefe7700eeac456
nicol <nicol@host.docker.internal>
2026-08-12 10:23:42 +0200

    updates

--- src/main/java/org/api/cardnexus/gui/components/MarketPlacePanel.java
+++ src/main/java/org/api/cardnexus/gui/components/MarketPlacePanel.java
@@ -1,8 +1,12 @@
 package org.api.cardnexus.gui.components;
 
+import java.awt.BorderLayout;
 import java.io.IOException;
+import java.util.Locale;
 
+import javax.swing.JButton;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTabbedPane;
 import javax.swing.JTable;
@@ -12,8 +16,10 @@ import org.apache.logging.log4j.Logger;
 import org.api.cardnexus.gui.model.MarketListTableModel;
 import org.api.cardnexus.model.AbstractProduct;
 import org.api.cardnexus.model.Seller;
+import org.api.cardnexus.model.requests.CartAddingRequest;
 import org.api.cardnexus.model.requests.HistoryRequest;
 import org.api.cardnexus.model.requests.MarketListRequest;
+import org.api.cardnexus.services.CartService;
 import org.api.cardnexus.services.PricesService;
 import org.api.cardnexus.services.ProductsService;
 
@@ -25,33 +31,80 @@ public class MarketPlacePanel extends JTabbedPane {
     private MarketVariationPanel marketVariationsPanel;
     private PriceHistoryPanel priceHistoryPanel;
     
-    private transient ProductsService service;
+    private transient ProductsService pservice;
     private transient PricesService pService;
+    private transient CartService cService;
     
     public MarketPlacePanel() {
 	
-	service = new ProductsService();
+	pservice = new ProductsService();
 	pService = new PricesService();
+	cService = new CartService();
+	
+	
 	modelMarketList = new MarketListTableModel();
 	var tableMarketLists = new JTable(modelMarketList);
 	marketVariationsPanel = new MarketVariationPanel();
 	priceHistoryPanel = new PriceHistoryPanel();
 	
+	var panelMarketList = new JPanel();
+	var panelMarketListCommands = new JPanel();
+	var btnAddCart = new JButton("Add to Cart");
 	
 	tableMarketLists.setDefaultRenderer(Seller.class, (JTable _, Object value, boolean _, boolean _,int _, int _) -> {
 	    	var ex = (Seller)value;
 	    	return new JLabel(ex.username());
 	});
 	
-	addTab("Nexus MarketList",new JScrollPane(tableMarketLists));
+	
+	btnAddCart.setEnabled(false);  
+	panelMarketList.setLayout(new BorderLayout());
+	
+	panelMarketListCommands.add(btnAddCart);
+	
+	panelMarketList.add(new JScrollPane(tableMarketLists), BorderLayout.CENTER);
+	panelMarketList.add(new JScrollPane(panelMarketListCommands), BorderLayout.EAST);
+	
+	
+	addTab("Nexus MarketList",panelMarketList);
 	addTab("Markets",marketVariationsPanel);
 	addTab("History", priceHistoryPanel);
+	
+	
+	tableMarketLists.getSelectionModel().addListSelectionListener(e->{
+	    if (!e.getValueIsAdjusting()) 
+    	    {
+    		int row = tableMarketLists.convertRowIndexToModel(tableMarketLists.getSelectedRow());
+    		btnAddCart.setEnabled(row>-1);
+    	    }
+	});
+	
+	btnAddCart.addActionListener(_->{
+	    int row = tableMarketLists.convertRowIndexToModel(tableMarketLists.getSelectedRow());
+	    if(row>-1)
+		{
+		    try {
+			 var id = modelMarketList.getValueAt(row, 0).toString();
+			cService.addItem(CartAddingRequest.create().setDeliveryCountry(Locale.getDefault().getCountry()).addItem(id, 1));
+		    } catch (Exception e1) {
+			logger.error(e1);
+		    }
+		}
+		
+		
+	    
+	});
+	
+	
+	
+	
+	
     }
     
     public void init(AbstractProduct p)
     {
 	try {
-	    modelMarketList.init(service.listMarketListing(MarketListRequest.create().setProductId(p.getId())));
+	    modelMarketList.init(pservice.listMarketListing(MarketListRequest.create().setProductId(p.getId())));
 	    marketVariationsPanel.init(p.getPrices());
 	    
 	    priceHistoryPanel.init(pService.getHistoryPrice(HistoryRequest.create().setIdProduct(p.getId())));
